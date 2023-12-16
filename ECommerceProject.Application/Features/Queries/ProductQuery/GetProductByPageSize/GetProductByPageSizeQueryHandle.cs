1ab915ebb6748a3b8d4d542cb117b39d9e05ffbc
Muhammed Dereli <muhammed.dereli@bilgeadamboost.com>
2023-12-16 15:45:28 +0300

    .

---
+++ ECommerceProject.Application/Features/Queries/ProductQuery/GetProductByPageSize/GetProductByPageSizeQueryHandle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ECommerceProject.Application.DTOs;
+using ECommerceProject.Application.FluentValidators.ProductValidators;
+using ECommerceProject.Application.Helpers;
+using ECommerceProject.Application.Repository.Interface.ProductRepo;
+using ECommerceProject.Domain.Ultilities.Results;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceProject.Application.Features.Queries.ProductQuery.GetProductByPageSize
+{
+    public class GetProductByPageSizeQueryHandle : IRequestHandler<GetProductByPageSizeQueryRequest, Result>
+    {
+       private readonly IProductReadRepository _productReadRepository;
+       private readonly IMapper _mapper;
+       private readonly IValidatorHelper _validatorHelper;
+
+        public GetProductByPageSizeQueryHandle(IProductReadRepository productReadRepository, IMapper mapper, IValidatorHelper validatorHelper)
+        {
+            _productReadRepository = productReadRepository;
+            _mapper = mapper;
+            _validatorHelper = validatorHelper;
+        }
+
+
+        public async Task<Result> Handle(GetProductByPageSizeQueryRequest request,
+            CancellationToken cancellationToken)
+        {
+            return await _validatorHelper.ValidateAndHandle(request, new GetProductByPageSizeQueryRequestValidator(),
+                async (validatedRequest) =>
+                {
+                    var totalProduct = _productReadRepository.GetProductByPageSize(false);
+
+                    var products = totalProduct.Skip(request.Page * request.Size).Take(request.Size)
+                        .Include(p => p.ProductImageFiles);
+                    var productDtos = _mapper.Map<IEnumerable<GetProductByPageSizeDto>>(products).ToList();
+
+                    return new SuccessDataResult<GetProductByPageSizeQueryResponse>(new GetProductByPageSizeQueryResponse(totalProduct.Count(), productDtos), "Listeleme Başarılı");
+
+                });
+
+
+        }
+
+
+    }
+
+   
+}
---
+++ ECommerceProject.Application/Features/Queries/ProductQuery/GetProductByPageSize/GetProductByPageSizeQueryResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceProject.Application.DTOs;
+using ECommerceProject.Domain.Concrete;
+
+namespace ECommerceProject.Application.Features.Queries.ProductQuery.GetProductByPageSize
+{
+    public class GetProductByPageSizeQueryResponse
+    {
+       
+        public int TotalProductCount { get; set; }
+        public List<GetProductByPageSizeDto>? Products { get; set; }
+        public GetProductByPageSizeQueryResponse(int totalProductCount, List<GetProductByPageSizeDto>? product )
+        {
+            TotalProductCount = totalProductCount;
+            Products = product;
+        }
+    }
+}
